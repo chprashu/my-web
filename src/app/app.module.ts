152e956b00e9c331c9caa37ce4c3db76aa2c6100
prashanth ch. <prashanth@chegus.com>
2023-12-27 17:47:16 +0530

    header section started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/header/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/header/footer/footer.component';
+import { DashboardComponent } from './components/header/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
