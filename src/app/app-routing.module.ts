83545a0b2df5217e6ffa84d8fabe9d14530053c8
prashanth ch. <prashanth@chegus.com>
2023-12-28 16:18:59 +0530

    design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { CareerComponent } from './components/pages/career/career.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"career", component:CareerComponent},
+  {path:"login", component:LoginComponent},
+  {path:"order", component:OrderComponent},
+  {path:"product", component:ProductsComponent},
+  {path:"signup", component:SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
