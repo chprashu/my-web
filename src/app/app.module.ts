be10fea1c10474ec9c69d75875df23c417e12387
prashanth ch. <prashanth@chegus.com>
2023-12-28 14:41:08 +0530

    nav-bar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/header/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/header/footer/footer.component';
 import { DashboardComponent } from './components/header/dashboard/dashboard.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProductsComponent } from './components/pages/products/products.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { CareerComponent } from './components/pages/career/career.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { SignUpComponent } from './components/pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    ProductsComponent,
+    OrderComponent,
+    CareerComponent,
+    AboutComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
